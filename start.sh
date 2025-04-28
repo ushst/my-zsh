95e9b9c994896dc8205cb3fe9ae0a8d8876552f9
ushastoe <40743392+krolchonok@users.noreply.github.com>
2025-04-28 15:51:50 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-apt install git zsh -y
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
